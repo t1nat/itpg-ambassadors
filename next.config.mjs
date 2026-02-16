302bde7c42e25fb56152ea4f184db4fb6c74765c
t1nat <tunchevatina@gmail.com>
2026-02-16 20:54:02 +0200

    fix(ambassadors): photos upload

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,18 @@ const nextConfig = {
         hostname: 'bgmqgozofllltfvauwhv.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
     ],
   },
 };
